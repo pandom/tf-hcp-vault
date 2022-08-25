9233018b4fdef0aac48098a81765443a7e597c85
pandom <pandom@users.noreply.github.com>
2022-08-25 16:36:55 +1000

    remove tfc

--- secrets_tf.tf
+++ secrets_tf.tf
@@ -1,30 +1,30 @@
-# This creates the team in terraform cloud
-resource "tfe_team" "team" {
-  name = "vault-team-token"
-  organization = var.tfe_organization
-}
+# # This creates the team in terraform cloud
+# resource "tfe_team" "team" {
+#   name = "vault-team-token"
+#   organization = var.tfe_organization
+# }
 
-## Creating the Vault Backend
-resource "vault_terraform_cloud_secret_backend" "tfc" {
-  backend = "tfc"
-  token = var.tfe_token
-}
+# ## Creating the Vault Backend
+# resource "vault_terraform_cloud_secret_backend" "tfc" {
+#   backend = "tfc"
+#   token = var.tfe_token
+# }
 
-resource "vault_terraform_cloud_secret_role" "this" {
-	backend  = vault_terraform_cloud_secret_backend.tfc.backend
-	name     = tfe_team.team.name
-	team_id  = tfe_team.team.id
-	ttl      = 300
-	max_ttl  = 900
-}
+# resource "vault_terraform_cloud_secret_role" "this" {
+# 	backend  = vault_terraform_cloud_secret_backend.tfc.backend
+# 	name     = tfe_team.team.name
+# 	team_id  = tfe_team.team.id
+# 	ttl      = 300
+# 	max_ttl  = 900
+# }
 
-resource "vault_policy" "tfc" {
-	name = 	tfe_team.team.name
+# resource "vault_policy" "tfc" {
+# 	name = 	tfe_team.team.name
 
-	policy =<<EOH
-path "${tfe_team.team.id}" {
-	capabilities = ["read"]
-}
-EOH
+# 	policy =<<EOH
+# path "${tfe_team.team.id}" {
+# 	capabilities = ["read"]
+# }
+# EOH
 
-}
+# }
