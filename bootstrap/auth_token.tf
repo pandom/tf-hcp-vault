75a0dcb04e1795b1a12e22e770ffdae082ceccb1
pandom <pandom@users.noreply.github.com>
2022-08-25 16:49:06 +1000

    Adding correct role

--- bootstrap/auth_token.tf
+++ bootstrap/auth_token.tf
@@ -1,6 +1,7 @@
 resource "vault_token" "token" {
   display_name = "tf-service-account"
   policies = ["hcp-root"]
+  role_name = "hcp-root"
   renewable = true
   ttl = "500h"
   no_default_policy = true
