179bed6bb458535888fae8ed280a365bd7d0e303
pandom <pandom@users.noreply.github.com>
2022-08-31 19:01:35 +1000

    Clean up bound_claims

--- auth_jwt.tf
+++ auth_jwt.tf
@@ -11,7 +11,7 @@ resource "vault_jwt_auth_backend_role" "jwt_hcp_role" {
     backend = vault_jwt_auth_backend.github.path
     role_name = "packer-action"
     bound_claims = { 
-        "repositories" = "pandom/packer-ubuntu",
+        "repositories" = "pandom/packer-ubuntu"
     }
     bound_audiences = ["sigstore"]
     role_type = "jwt"
