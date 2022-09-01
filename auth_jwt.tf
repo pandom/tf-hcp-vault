f23cb46b6acc83375fd9ea169ba9f0b8066f072d
pandom <pandom@users.noreply.github.com>
2022-09-01 15:35:38 +1000

    Test

--- auth_jwt.tf
+++ auth_jwt.tf
@@ -5,12 +5,19 @@ resource "vault_jwt_auth_backend" "github" {
   bound_issuer       = "https://token.actions.githubusercontent.com"
 }
 
+resource "vault_jwt_auth_backend" "gh_actions" {
+  description        = "Github JWT auth backend"
+  path               = "gh-actions"
+  oidc_discovery_url = "https://token.actions.githubusercontent.com"
+  bound_issuer       = "https://token.actions.githubusercontent.com"
+}
+
 ### This requires a key-value mounted to secret/data/
 ## Source: https://docs.github.com/en/actions/deployment/security-hardening-your-deployments/configuring-openid-connect-in-hashicorp-vault
 resource "vault_jwt_auth_backend_role" "jwt_hcp_role" {
   backend         = vault_jwt_auth_backend.github.path
   role_name       = "packer-action"
-  bound_audiences = ["sigstore"]
+  bound_audiences = ["https://github.com/pandom"]
   bound_claims = {
     "repository" = "pandom/gha-vault-jwt"
     "ref"        = "refs/heads/main"
