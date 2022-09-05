09bb6db058511a0f06b1e50e27c270bf2d260fab
pandom <pandom@users.noreply.github.com>
2022-09-05 16:37:24 +1000

    Adds additional KV stores

--- secrets_kv.tf
+++ secrets_kv.tf
@@ -7,6 +7,24 @@ resource "vault_mount" "mount" {
   description = "HCP Secrets"
 }
 
+resource "vault_mount" "mount_aws" {
+  path = "aws"
+  type = "kv"
+  options = {
+    version = "2"
+  }
+  description = "AWS Secrets"
+}
+
+resource "vault_mount" "mount_gcp" {
+  path = "gcp"
+  type = "kv"
+  options = {
+    version = "2"
+  }
+  description = "GCP Secrets"
+}
+
 # Seeding of HCP Vault credentials can be done via Obsidian bootstrap
 # obsidian://open?vault=hive&file=Vault%2FVault%20Lab%20Bootstrap
 # vault kv put hcp/creds hcp_client_id=$(echo $HCP_CLIENT_ID), hcp_client_secret=$(echo $HCP_CLIENT_SECRET)
