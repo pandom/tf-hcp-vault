3c0709426170a18988b09a577adeeedc26d685b3
pandom <pandom@users.noreply.github.com>
2022-11-02 13:07:42 +1100

    Adjustment to working

--- auth_tfc.tf
+++ auth_tfc.tf
@@ -1,57 +1,43 @@
-# ### This could be a list that's iterated over. For each user..
 data "tfe_organization_membership" "burkey" {
-  organization  = "burkey"
-  #organization  = var.tfe_organization
-  username = "burkey"
+  organization  = var.tfe_organization
+  email = "burkey@hashicorp.com"
 }
-
 data "tfe_organization_membership" "go" {
-organization  = "burkey"
-  #organization  = var.tfe_organization
-  username = "go"
+  organization  = var.tfe_organization
+  email = "go@hashicorp.com"
 }
 
-# ## Create Backend
-# resource "vault_terraform_cloud_secret_backend" "tfc_backend" {
-#   backend     = "terraform"
-#   description = "TFC Backend for ${var.tfe_organization}"
-#   #token       = var.tfe_token
-# }
+resource "vault_terraform_cloud_secret_backend" "test" {
+  backend     = "terraform"
+  description = "TFC Backend"
+  token       = var.tfe_token
+}
 
-## Creates Team
+resource "vault_terraform_cloud_secret_role" "example" {
+  backend      = vault_terraform_cloud_secret_backend.test.backend
+  name         = "vsphere_read"
+  organization = "burkey"
+  team_id      = tfe_team.vsphere_read.id
+  depends_on = [
+    tfe_team.vsphere_read
+  ]
+}
+
+## Creates Group
 resource "tfe_team" "vsphere_read" {
   name = "vsphere_read"
-  organization  = var.tfe_organization
+  organization = var.tfe_organization
+  sso_team_id = "INSERT_TEAM_ID"
 }
+
+## Assign Users to Group
 
 resource "tfe_team_organization_member" "burkey" {
-    team_id = tfe_team.vsphere_read.id
-    organization_membership_id = data.tfe_organization_membership.burkey.id 
+  team_id = tfe_team.vsphere_read.id
+  organization_membership_id = data.tfe_organization_membership.burkey.id
 }
+
 resource "tfe_team_organization_member" "go" {
-    team_id = tfe_team.vsphere_read.id
-    organization_membership_id = data.tfe_organization_membership.go.id 
+  team_id = tfe_team.vsphere_read.id
+  organization_membership_id = data.tfe_organization_membership.go.id
 }
-
-# ## Create Role
-# # resource "vault_terraform_cloud_secret_role" "example" {
-# #   backend      = vault_terraform_cloud_secret_backend.tfc_backend.backend
-# #   name         = "vsphere_read"
-# #   organization  = var.tfe_organization
-# #   #organization = var.tfe_organization
-# #   team_id      = tfe_team.vsphere_read.id
-# #   depends_on = [
-# #     tfe_team.vsphere_read
-# #   ]
-# #}
-
-# ## Map Role and Policy
-# ## Assign Users to Group
-
-# resource "tfe_team_organization_members" "vsphere_read" {
-#       team_id      = tfe_team.vsphere_read.id
-#       organization_membership_ids = [
-#         data.tfe_organization_membership.burkey.id,
-#         data.tfe_organization_membership.go.id
-#       ]
-# }
