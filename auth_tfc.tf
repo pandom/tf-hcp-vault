cd2ba7cf431c838142d535510bb9b0fd252176c4
pandom <pandom@users.noreply.github.com>
2022-11-02 13:35:04 +1100

    Freedom

--- auth_tfc.tf
+++ auth_tfc.tf
@@ -1,48 +1,48 @@
-data "tfe_organization" "org" {
-  name = var.tfe_organization
-}
-data "tfe_organization_membership" "burkey" {
-  organization = data.tfe_organization.org.name
-  #email = "burkey@hashicorp.com"
-  username = "burkey"
-}
-data "tfe_organization_membership" "go" {
-  organization = data.tfe_organization.org.name
-  #email = "go@hashicorp.com"
-  username = "go"
-}
+# data "tfe_organization" "org" {
+#   name = var.tfe_organization
+# }
+# data "tfe_organization_membership" "burkey" {
+#   organization = data.tfe_organization.org.name
+#   #email = "burkey@hashicorp.com"
+#   username = "burkey"
+# }
+# data "tfe_organization_membership" "go" {
+#   organization = data.tfe_organization.org.name
+#   #email = "go@hashicorp.com"
+#   username = "go"
+# }
 
-resource "vault_terraform_cloud_secret_backend" "test" {
-  backend     = "terraform"
-  description = "TFC Backend"
-  token       = var.tfe_token
-}
+# resource "vault_terraform_cloud_secret_backend" "test" {
+#   backend     = "terraform"
+#   description = "TFC Backend"
+#   token       = var.tfe_token
+# }
 
-resource "vault_terraform_cloud_secret_role" "example" {
-  backend      = vault_terraform_cloud_secret_backend.test.backend
-  name         = "vsphere_read"
-  organization = data.tfe_organization.org.name
-  team_id      = tfe_team.vsphere_read.id
-  depends_on = [
-    tfe_team.vsphere_read
-  ]
-}
+# resource "vault_terraform_cloud_secret_role" "example" {
+#   backend      = vault_terraform_cloud_secret_backend.test.backend
+#   name         = "vsphere_read"
+#   organization = data.tfe_organization.org.name
+#   team_id      = tfe_team.vsphere_read.id
+#   depends_on = [
+#     tfe_team.vsphere_read
+#   ]
+# }
 
-## Creates Group
-resource "tfe_team" "vsphere_read" {
-  name         = "vsphere_read"
-  organization = var.tfe_organization
-  sso_team_id  = "INSERT_TEAM_ID"
-}
+# ## Creates Group
+# resource "tfe_team" "vsphere_read" {
+#   name         = "vsphere_read"
+#   organization = var.tfe_organization
+#   sso_team_id  = "INSERT_TEAM_ID"
+# }
 
-## Assign Users to Group
+# ## Assign Users to Group
 
-resource "tfe_team_organization_member" "burkey" {
-  team_id                    = tfe_team.vsphere_read.id
-  organization_membership_id = data.tfe_organization_membership.burkey.id
-}
+# resource "tfe_team_organization_member" "burkey" {
+#   team_id                    = tfe_team.vsphere_read.id
+#   organization_membership_id = data.tfe_organization_membership.burkey.id
+# }
 
-resource "tfe_team_organization_member" "go" {
-  team_id                    = tfe_team.vsphere_read.id
-  organization_membership_id = data.tfe_organization_membership.go.id
-}
+# resource "tfe_team_organization_member" "go" {
+#   team_id                    = tfe_team.vsphere_read.id
+#   organization_membership_id = data.tfe_organization_membership.go.id
+# }
