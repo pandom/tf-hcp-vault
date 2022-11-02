7037f5953635c99d18aecf94568a0f4bb96c4fee
pandom <pandom@users.noreply.github.com>
2022-11-02 12:32:05 +1100

    org memberships

--- auth_tfc.tf
+++ auth_tfc.tf
@@ -20,14 +20,12 @@ organization  = var.tfe_organization
 resource "tfe_team" "vsphere_read" {
   name = "vsphere_read"
   organization  = var.tfe_organization
-  #organization = var.tfe_organization
 }
 
-resource "tfe_team_member" "burkey" {
-    team_id = tfe_team.vsphere_read
-    tfe_team_member = data.tfe_organization_membership.burkey.id
-  
-}
+# resource "tfe_team_organization_member" "burkey" {
+#     team_id = tfe_team.vsphere_read.id
+#     tfe_team_member = data.tfe_organization_membership.burkey.id
+# }
 
 # ## Create Role
 # # resource "vault_terraform_cloud_secret_role" "example" {
@@ -44,10 +42,10 @@ resource "tfe_team_member" "burkey" {
 # ## Map Role and Policy
 # ## Assign Users to Group
 
-# resource "tfe_team_organization_members" "vsphere_read" {
-#       team_id      = tfe_team.vsphere_read.id
-#       organization_membership_ids = [
-#         data.tfe_organization_membership.burkey.id,
-#         data.tfe_organization_membership.go.id
-#       ]
-# }
+resource "tfe_team_organization_members" "vsphere_read" {
+      team_id      = tfe_team.vsphere_read.id
+      organization_membership_ids = [
+        data.tfe_organization_membership.burkey.id,
+        data.tfe_organization_membership.go.id
+      ]
+}
